7c47c1f24b786b2c9bd41167228807ce6a0e7c4f
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-04-15 09:36:31 +0200

    Prepared to start new project

---
+++ src/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    };
+
+    *, *:before, *:after {
+        box-sizing: inherit;    
+    };
+
+    body {
+        background-color: ${({ theme }) => theme.color.gray};
+    }
+`
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    color: {
+        gray: "#808080",
+    },
+    breakpoints: {
+
+    },
+}
