eeb1b5d73ad7ea1bc3a10268646ad7aef5dafda5
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-04-16 12:44:33 +0200

    added calendar grid to main app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import { Title } from "./styled";
+import { CalendarGrid } from "./CalendarGrid/CalendarGrid";
 
 function App() {
   return (
     <div className="App">
+      <Title>
+        Calendar
+      </Title>
+      <CalendarGrid />
     </div>
   );
 }
